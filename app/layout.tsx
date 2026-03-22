723ce0f113e38e5f289cfabe766be3ff6cc32c4b
Ken Ho <dearken618@gmail.com>
2026-03-23 02:08:09 +0800

    W03 Next.js application complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
+import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+};
 
 export default function RootLayout({
   children,
